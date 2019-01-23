a68cf4b95979278d01822fd26c9f1d46201ecda9
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-01-23 14:08:11 +0530

    Create Clock.c

---
+++ Clock.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+void main(){
+int a=3;
+if(a>0){
+printf("the number is positive");}
+elseif(a<0){
+printf("the number is negative");}
+else{
+printf("zero");}
+getch();}
