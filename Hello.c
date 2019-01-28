4aca1932bec065c2ad48fc41ee58d2848f74d4b7
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-01-28 20:28:56 +0530

    Update and rename Hello.py to Hello.c

---
+++ Hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int a,b,c;
+if(a>c)
+printf("a is larger");
+elseif(b>c)
+printf("b is larger");
+else 
+printf("c is larger");
+getch();
+       
+        
+        
+
