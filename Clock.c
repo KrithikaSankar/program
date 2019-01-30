e78caec44c87bc3a579bf13ecca32b1a1db80f97
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-01-30 19:46:50 +0530

    Update Clock.c

--- Clock.c
+++ Clock.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
 int main(void){
-int i,n;
+int i,n,sum=0;
 printf("enter the value of n");
 scanf("%d",&n);
 for(i=1;i<=n;i++){
-printf("hello world");
+sum=sum+i; 
+printf("%d",sum);
   return 0;
 }}
 
