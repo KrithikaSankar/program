a7d9c42bf56253e2b60efd4c8fd84f8728d68c72
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-01-30 20:28:16 +0530

    Update and rename digit.c to digits.c

---
+++ digits.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+ long long num;
+int count=0;
+printf("enter any number");//krithika
+scanf("%lld",&num);
+count=log10(num)+1;
+ printf("total digits:%d",count);
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
