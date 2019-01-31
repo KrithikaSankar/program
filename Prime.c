6422effd93d27a8fc31c30e8c8445f54a7902932
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-01-31 19:45:47 +0530

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -4,17 +4,17 @@ int main()
 int n,i,Yes,No; 
 printf("%d",n); 
 scanf("%d",&n); 
-for(i=2;i<=n/2;++i) 
+for(i=2;i<=n/2;++i) {
 if(n%i==0) 
 { 
 break; 
 } 
 if(i>n/2){ 
-printf("%d", Yes); 
-else 
+printf("%d", Yes); }
+else {
 printf("%d", No); 
   return 0;
-}}
+}}}
 
 
 
