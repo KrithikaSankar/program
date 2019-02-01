c4dbdd4cac40ba03d9cafa22758dfb0b43aaeb97
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-01 14:55:22 +0530

    Update and rename interval.c to Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<math.h> 
+int main(void) 
+{ 
+int n, sum = 0, rem = 0, cube = 0, temp; 
+char Yes, No; 
+printf("%d", n); 
+scanf("%d", &n); 
+temp = n; 
+while(n != 0) 
+{ 
+   rem = n % 10; 
+   cube = pow(rem, 3); 
+   sum = sum + cube; 
+   n = n / 10; 
+   } 
+ if(sum == temp) 
+    printf("%s", Yes); 
+   else 
+    printf("%s", No); 
+ }
+  
+
+
+
+
+   
+   
+
+      
+      
+         
+        
+        
+   
+   
+      
+   
+
+   
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+
+
+
+
+       
+        
+        
+
