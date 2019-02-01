d06f08e777c44423b661184defbf2b49ec8fdfa9
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-01 15:37:54 +0530

    Update and rename Odd to Fact.c

---
+++ Fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int main(void) 
+{ 
+int i, fact = 1, n; 
+printf("%d", n); 
+scanf("%d", &n); 
+if(n <= 0) 
+fact = 1; 
+else 
+{ 
+for(i = 1; i <= n; i++) 
+{ 
+fact = fact * i; 
+} 
+} 
+printf("%d", fact); 
+} 
