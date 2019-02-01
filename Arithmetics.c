6cde7148c575cde2bb84caae740c4b0ed1151c82
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-01 18:19:53 +0530

    Update and rename Arithmetic.c to Arithmetics.c

---
+++ Arithmetics.c
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+#include <math.h> 
+int main(void) 
+  { 
+int a, d, n, i, sum = 0; 
+printf("%d", n); 
+scanf("%d", &n); 
+printf("%d", a); 
+scanf("%d", &a); 
+printf("%d", d); 
+scanf("%d", &d); 
+for(i=1;i<=n;i++)
+sum=sum+(a+(i*d)); 
+} 
+printf("%d", sum);
+return 0; 
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
