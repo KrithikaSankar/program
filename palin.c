7c3735723847fd90c9054352065f859ca28f2669
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-04 11:27:38 +0530

    Update and rename Class.c to palin.c

---
+++ palin.c
@@ -0,0 +1,18 @@
+
+include <stdio.h>
+
+int main(void) {
+int t,n,reverse=0;
+t=n;
+while(t!=0)
+{
+	reverse=reverse*10;
+	reverse=reverse+t%10;
+	t=t/10;
+	}
+if(n==reverse)
+printf("yes");
+else
+printf("No");
+	return 0;
+}
