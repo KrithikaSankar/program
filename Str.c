8e1e38ad680704567835b8d265a7c18c6aa2a9d4
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-04 18:55:39 +0530

    Update and rename Sum.c to Str.c

--- Str.c
+++ Str.c
@@ -1,16 +1,34 @@
 
 
+
+
 #include <stdio.h>
 
 int main(void) {
-int a, b, sum;
-scanf("%d %d",&a,&b);
-sum= a+b;
-printf("%d",sum);
+int i=0, j=0;
+char str1[25],str2[25];
+scanf("%s %s",str1,str2);
+while(str1[i]!='\0')
+i++;
+while(str2[j]!='\0')
+{
+str1[i]=str2[j];
+j++;
+i++;
+}
+str1[i]='\0';
+printf("%s",str1);
+return 0;
+}
+
+
+
+
+
 
 	
-	return 0;
-}
+	
+
 
 
 
