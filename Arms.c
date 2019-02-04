75d89205be4960c421e0b1e1c4864ebdcfb6f3bf
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-04 14:09:56 +0530

    Update and rename Arithmeticss.c to Arms.c

---
+++ Arms.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+int i, n, t1=0, t2=1, t3;
+scanf("%d",&n);
+for(i=1;i<=n;++i){
+	printf("%d",t1);
+t3=t1+t2;
+t1=t2;
+t2=t3;}
+
+	return 0;
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
