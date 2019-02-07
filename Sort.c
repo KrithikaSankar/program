f32e1f4d9e6ff3b161cfefde645d3f9227a0478f
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-02-07 07:07:24 +0530

    Create Sort.c

---
+++ Sort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+int a[200],n,i,largest;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+largest=a[0]; 
+for(i=0;i<n;i++)
+{
+	if(largest<a[i])
+	{
+		largest=a[i];
+	}}
+	printf("%d",largest);
+	return 0;
+}
