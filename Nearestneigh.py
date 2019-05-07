e1da9435518cbdaba7f6b5147463f673986b125b
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-05-07 20:53:56 +0530

    Update Nearestneigh.py

--- Nearestneigh.py
+++ Nearestneigh.py
@@ -2,5 +2,5 @@ n,k=map(int,input().split())
 l=[[abs(i-k),i]for i in list(map(int,input().split()))]
 l.sort()
 l=l[1:]
-z=[i[1] for i in l[:3]]
+z=[i[1] for i in l[:3]] //Kirthika
 print(*z,sep=' ')
