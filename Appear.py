f029bd7f6bb67b3234e1aa6e1861a80e0221f747
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-05-09 14:53:37 +0530

    Update Appear.py

--- Appear.py
+++ Appear.py
@@ -1,7 +1,7 @@
 n, k=map(int, input().split())
 a=[]
 for _ in range(n):
-	b = set(map(int, input().split()))  \\krithika
+	b = set(map(int, input().split()))  
 	a.append(b)
 t=set.intersection(*a)
 print(*t)
