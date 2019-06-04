3f8fe24ab0eea654a28b0ee17ebbd44919d0a815
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-06-04 13:07:00 +0530

    Create stringpalind.py

---
+++ stringpalind.py
@@ -0,0 +1,16 @@
+#krithika
+Q=input()
+if Q==Q[::-1]:
+    print("yes")
+else:
+    a=Q.strip("0")
+    
+    if a==a[::-1]:
+        print("yes")
+    else:
+        a=Q.lstrip("0")
+        
+        if a==a[::-1]:
+            print("yes")
+        else:
+            print("no")
