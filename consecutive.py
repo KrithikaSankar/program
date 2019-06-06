3e663ba27e9a1c4b3b282c5bad3b7a5bbe1993c6
KrithikaSankar <46593333+KrithikaSankar@users.noreply.github.com>
2019-06-05 21:26:41 -0700

    Create consecutive.py

---
+++ consecutive.py
@@ -0,0 +1,12 @@
+#krithika
+a,b=map(str,input().split())
+count=0
+for i in range(0,len(a)):
+	for j in range(0,len(b)):
+		if a[i]==b[j]:
+			count+=1
+if count>=2:
+	print("yes")
+else:
+	print("no")
+© 2019 GitHub, Inc.
